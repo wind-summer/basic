4beb62333c07300b571a79a968c6b0a470b54d65
wenlongfei <wenlongfei@itangquan.com>
2018-10-18 18:21:08 +0800

    添加生成验证码接口

--- admin/src/main/java/com.basic/admin/module/sys/SysLoginController.java
+++ admin/src/main/java/com.basic/admin/module/sys/SysLoginController.java
@@ -5,14 +5,23 @@ import com.basic.core.module.sys.entity.request.SysLogin;
 import com.basic.core.module.sys.service.SysUserService;
 import com.basic.core.module.sys.service.SysUserTokenService;
 import com.basic.core.utils.ApiResult;
+import com.basic.core.utils.ShiroUtils;
+import com.google.code.kaptcha.Constants;
+import com.google.code.kaptcha.Producer;
+import com.google.code.kaptcha.impl.DefaultKaptcha;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
+import org.apache.commons.io.IOUtils;
 import org.apache.shiro.crypto.hash.Sha256Hash;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.imageio.ImageIO;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 /**
  * 登陆控制器
@@ -24,8 +33,27 @@ import org.springframework.web.bind.annotation.RestController;
 @Api(description = "登陆管理")
 public class SysLoginController {
 
+    private Producer producer;
     private SysUserService sysUserService;
     private SysUserTokenService sysUserTokenService;
+
+    @ApiOperation(value = "获取验证码图片-接口")
+    @RequestMapping("/captcha.jpg")
+    public void captcha(HttpServletResponse response) throws ServletException,IOException {
+        response.setHeader("Cache-Control", "no-store, no-cache");
+        response.setContentType("image/jpeg");
+
+        //生成文字验证码
+        String text = producer.createText();
+        //生成图片验证码
+        BufferedImage image = producer.createImage(text);
+        //保存到shiro session
+        ShiroUtils.setSessionAttribute(Constants.KAPTCHA_SESSION_KEY, text);
+
+        ServletOutputStream out = response.getOutputStream();
+        ImageIO.write(image, "jpg", out);
+        IOUtils.closeQuietly(out);
+    }
 
     /**
      * 用户登陆操作
